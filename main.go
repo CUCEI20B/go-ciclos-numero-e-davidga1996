8da9d5cacf303548d9a0adeef1d2a7c0384856dc
David <david_xd1996@live.com.mx>
2020-09-30 21:57:52 -0500

    primer commit

--- main.go
+++ main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var e float64
+	var exponencial, rev int
+
+	fmt.Print()
+	fmt.Scan(&rev)
+
+	e = 1
+	for i := 1; i <= rev; i++ {
+		exponencial = 1
+		for j := 1; j <= i; j++ {
+			exponencial = exponencial * j
+		}
+		e = e + 1/float64(exponencial)
+	}
+
+	fmt.Println(e)
 }
